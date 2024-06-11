c2742f1baa439b1da1c95dba6b0d23818abd652c
vladas9 <vladamza63@gmail.com>
2024-06-11 16:42:35 +0300

    final version

--- back/src/main.rs
+++ back/src/main.rs
@@ -14,49 +14,68 @@ use tokio_tungstenite::tungstenite::protocol::Message;
 struct Individual {
     id: u32,
     position: [i32; 2],
-    movement_speed: u32,
     grid_size: i32,
     is_infected: bool,
     infected_time: f32,
     alive: bool,
+    velocity: [f32; 2],
+    acceleration: [f32; 2],
+    max_speed: f32,
+    friction: f32,
+    immunity: f32,
 }
 
 impl Individual {
-    fn new(
-        id: u32,
-        position: [i32; 2],
-        movement_speed: u32,
-        grid_size: i32,
-        is_infected: bool,
-    ) -> Self {
+    fn new(id: u32, position: [i32; 2], max_speed: f32, grid_size: i32, is_infected: bool) -> Self {
         Individual {
             id,
             position,
-            movement_speed,
+            max_speed,
             grid_size,
             is_infected,
             infected_time: 0.0,
             alive: true,
+            velocity: [0.0, 0.0],
+            acceleration: [0.0, 0.0],
+            friction: 0.1,
+            immunity: 1.0,
         }
     }
 
     fn move_ind(&mut self) {
         let mut rng = rand::thread_rng();
-        let movement_speed = self.movement_speed as i32;
-        let movement = [
-            rng.gen_range(-movement_speed..=movement_speed),
-            rng.gen_range(-movement_speed..=movement_speed),
+        let movement_speed = self.max_speed;
+        self.acceleration = [
+            rng.gen_range(-movement_speed / 3.0..=movement_speed / 3.0),
+            rng.gen_range(-movement_speed / 3.0..=movement_speed / 3.0),
         ];
-        self.position[0] = (self.position[0] + movement[0]).rem_euclid(self.grid_size);
-        self.position[1] = (self.position[1] + movement[1]).rem_euclid(self.grid_size);
+        self.velocity[0] += self.acceleration[0];
+        self.velocity[1] += self.acceleration[1];
+
+        // Apply friction
+        self.velocity[0] *= 1.0 - self.friction;
+        self.velocity[1] *= 1.0 - self.friction;
+
+        // Limit speed
+        self.velocity[0] = self.velocity[0].clamp(-self.max_speed, self.max_speed);
+        self.velocity[1] = self.velocity[1].clamp(-self.max_speed, self.max_speed);
+
+        // Update position
+        self.position[0] =
+            (self.position[0] as f32 + self.velocity[0]).rem_euclid(self.grid_size as f32) as i32;
+        self.position[1] =
+            (self.position[1] as f32 + self.velocity[1]).rem_euclid(self.grid_size as f32) as i32;
     }
 
     fn dead_treated(&mut self, probability: f32) {
         let mut rng = rand::thread_rng();
-        if rng.gen_range(0.0..1.0) < E.powf(-probability * self.infected_time as f32) {
-            if self.infected_time > 4.0 {
+        if rng.gen_range(0.0..1.0)
+            < E.powf(-probability * self.infected_time as f32 * self.immunity)
+        {
+            if self.infected_time > rng.gen_range(22..29) as f32 {
                 self.is_infected = false;
                 self.infected_time = 0.0;
+                self.immunity /= 10.0;
             } else {
                 self.infected_time += 0.0017;
             }
@@ -100,7 +119,7 @@ async fn handle_connection(stream: TcpStream, client_id: usize) {
     while let Some(msg) = ws_receiver.next().await {
         let msg = msg.expect(format!("Error receiving message from client {}", client_id).as_str());
         println!("{}", msg);
-        let _map: HashMap<String, String> = match serde_json::from_str(&msg.to_string()) {
+        let map: HashMap<String, String> = match serde_json::from_str(&msg.to_string()) {
             Ok(map) => map,
             Err(e) => {
                 eprintln!("Error parsing message: {:?}", e);
@@ -108,14 +127,44 @@ async fn handle_connection(stream: TcpStream, client_id: usize) {
             }
         };
 
-        let grid_size = 500;
-        let movement_speed = 5;
-        let num_individuals = 100;
+        let grid_size: i32 = map
+            .get("grid_size")
+            .unwrap_or(&"500".to_string())
+            .parse()
+            .unwrap();
+        let max_speed: f32 = map
+            .get("movement_speed")
+            .unwrap_or(&"3.0".to_string())
+            .parse()
+            .unwrap();
+        let num_individuals: u32 = map
+            .get("num_individuals")
+            .unwrap_or(&"100".to_string())
+            .parse()
+            .unwrap();
         let initial_infected_index = 0;
-        let num_iterations = 100000;
-        let infection_radius = 20;
-        let infection_probability = 0.2;
-        let probability_of_dying = 0.000005;
+        let num_iterations: usize = map
+            .get("num_iterations")
+            .unwrap_or(&"1000".to_string())
+            .parse()
+            .unwrap();
+        let infection_radius: i32 = map
+            .get("infection_radius")
+            .unwrap_or(&"30".to_string())
+            .parse()
+            .unwrap();
+        let infection_probability: f32 = map
+            .get("infection_probability")
+            .unwrap_or(&"50".to_string())
+            .parse::<f32>()
+            .unwrap()
+            / 400.0;
+        let probability_of_dying: f32 = map
+            .get("probability_of_dying")
+            .unwrap_or(&"5".to_string())
+            .parse::<f32>()
+            .unwrap()
+            / 50000000.0;
 
         let mut population: Vec<Individual> = Vec::new();
         let mut rng = StdRng::from_entropy();
@@ -124,13 +173,13 @@ async fn handle_connection(stream: TcpStream, client_id: usize) {
             let individual = Individual::new(
                 i,
                 [rng.gen_range(0..grid_size), rng.gen_range(0..grid_size)],
-                movement_speed,
+                max_speed,
                 grid_size,
                 false,
             );
             population.push(individual);
         }
-        println!("{:?}", population[initial_infected_index].position);
+
         population[initial_infected_index].is_infected = true;
         for _ in 0..num_iterations * 60 {
             let json_message = serde_json::to_string(&population).unwrap();
@@ -155,7 +204,8 @@ async fn handle_connection(stream: TcpStream, client_id: usize) {
                         }
                         let dist = distance(population[i].position, population[j].position);
                         if dist <= infection_radius as f32
-                            && rng.gen_range(0.0..1.0) <= infection_probability
+                            && rng.gen_range(0.0..1.0)
+                                <= infection_probability * population[j].immunity
                         {
                             population[j].is_infected = true;
                         }
